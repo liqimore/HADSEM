0885a5207d04255c9b722f4c79b78ff2b1368fba
Ricky Li <at20s@icloud.com>
2019-03-13 12:44:11 +0800

    helloWorld

---
+++ src/main/java/com/codefog/hadsem/HadsemApplication.java
@@ -0,0 +1,13 @@
+package com.codefog.hadsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadsemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadsemApplication.class, args);
+	}
+
+}
